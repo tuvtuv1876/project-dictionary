2da9030d39c1671800e4bc0c16970c821a4207c3
tuvtuv1876 <87674025+tuvtuv1876@users.noreply.github.com>
2022-01-26 21:47:24 +0100

    updated synonym integration

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,15 +1,19 @@
 import React from "react";
 
 export default function Synonyms(props) {
-  return (
-    <div className="Synonyms">
-      {props.synonyms.map(function (synonym, index) {
-        return (
-          <ul key={index}>
-            <li>{synonym}</li>
-          </ul>
-        );
-      })}
-    </div>
-  );
+  if (props.synonyms) {
+    return (
+      <div className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return (
+            <ul key={index}>
+              <li>{synonym}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
