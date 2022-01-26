437f11e474e0ec48e6c7c1b14d2b3f5f6fa853eb
tuvtuv1876 <87674025+tuvtuv1876@users.noreply.github.com>
2022-01-26 22:24:14 +0100

    added phonetics integration

---
+++ src/Phonetics.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Phonetics(props) {
+  if (props.phonetics) {
+    return (
+      <div className="Phonetics">
+        {props.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              [{phonetic.text}]{" "}
+              <a href={phonetic.audio} alt="">
+                audio
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
--- src/Results.js
+++ src/Results.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        <Phonetics phonetics={props.results.phonetics} />
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
